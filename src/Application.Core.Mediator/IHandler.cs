c9f2b07ba82ad95b19dd7ba804b349ed808f50b8
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-11 20:00:58 +0100

    Add IHandle<TRequest> abstraction that has no response type (void/task)

--- src/Application.Core.Mediator/IHandler.cs
+++ src/Application.Core.Mediator/IHandler.cs
@@ -5,7 +5,7 @@
 /// </summary>
 /// <typeparam name="TRequest">The request type.</typeparam>
 /// <typeparam name="TResponse">The response type.</typeparam>
-public interface IHandler<in TRequest, TResponse>
+public interface IHandler<in TRequest, TResponse> where TRequest : IRequest<TResponse>
 {
     /// <summary>
     /// Handles the specified <paramref name="request"/> asynchronously.
@@ -14,4 +14,18 @@ public interface IHandler<in TRequest, TResponse>
     /// <param name="cancellationToken">The cancellation token.</param>
     /// <returns>The resulting <see cref="TResponse"/>.</returns>
     public Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken);
+}
+
+/// <summary>
+/// Represents a handler for a given request.
+/// </summary>
+/// <typeparam name="TRequest">The request type.</typeparam>
+public interface IHandler<in TRequest> where TRequest : IRequest
+{
+    /// <summary>
+    /// Handles the specified <paramref name="request"/> asynchronously.
+    /// </summary>
+    /// <param name="request">The request.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    public Task HandleAsync(TRequest request, CancellationToken cancellationToken);
 }
