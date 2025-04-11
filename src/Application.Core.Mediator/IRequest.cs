bf2c71c624ed318fbb0c64a7d7aec180988a8581
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-11 20:00:36 +0100

    Add IRequest, IRequest<TResponse> and IBaseRequest abstractions

---
+++ src/Application.Core.Mediator/IRequest.cs
@@ -0,0 +1,17 @@
+﻿namespace Application.Core.Mediator;
+
+/// <summary>
+/// Represents a request without a response type.
+/// </summary>
+public interface IRequest : IBaseRequest;
+
+/// <summary>
+/// Represents a request with a response of type <see cref="T:TResponse"/>.
+/// </summary>
+/// <typeparam name="TResponse">The response type.</typeparam>
+public interface IRequest<TResponse> : IBaseRequest;
+
+/// <summary>
+/// Marker interface for a base request.
+/// </summary>
+public interface IBaseRequest;
