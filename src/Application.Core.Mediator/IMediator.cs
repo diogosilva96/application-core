ecd9d66478871ebe8487737b7b2a0b1de2487e7a
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-11 20:01:07 +0100

    Introduce IMediator abstraction

---
+++ src/Application.Core.Mediator/IMediator.cs
@@ -0,0 +1,23 @@
+﻿namespace Application.Core.Mediator;
+
+/// <summary>
+/// Represents a mediator component abstraction.
+/// </summary>
+public interface IMediator
+{
+    /// <summary>
+    /// Handles the given <paramref name="request"/>.
+    /// </summary>
+    /// <param name="request">The request to handle.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <typeparam name="TResponse">The response type.</typeparam>
+    /// <returns>The resulting response of type <see cref="T:TResponse"/>.</returns>
+    public Task<TResponse> HandleAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    
+    /// <summary>
+    /// Handles the given <paramref name="request"/>.
+    /// </summary>
+    /// <param name="request">The request to handle.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    public Task HandleAsync(IRequest request, CancellationToken cancellationToken = default);
+}
