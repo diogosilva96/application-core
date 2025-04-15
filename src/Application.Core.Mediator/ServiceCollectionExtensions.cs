ebd0e4770c1b4b971cf554e522bb78988cfcad2b
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-15 18:46:45 +0100

    Fix summary

--- src/Application.Core.Mediator/ServiceCollectionExtensions.cs
+++ src/Application.Core.Mediator/ServiceCollectionExtensions.cs
@@ -11,7 +11,7 @@ public static class ServiceCollectionExtensions
     /// Adds the mediator service(s) to the given <paramref name="serviceCollection" />.
     /// </summary>
     /// <param name="serviceCollection">The service collection.</param>
-    /// <param name="configureMediator">The action for configuring the meidiator.</param>
+    /// <param name="configureMediator">The action for configuring the mediator.</param>
     /// <returns>The <see cref="IServiceCollection" /> with the added service(s).</returns>
     public static IServiceCollection AddMediator(this IServiceCollection serviceCollection, Action<MediatorBuilder>? configureMediator = null)
     {
