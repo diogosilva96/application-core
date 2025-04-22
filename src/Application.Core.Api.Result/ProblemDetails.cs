d42da254ba69e77adb8ab48709d33d56265b688e
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-22 20:30:18 +0100

    Introduce base ProblemDetails type

---
+++ src/Application.Core.Api.Result/ProblemDetails.cs
@@ -0,0 +1,48 @@
+﻿using Application.Core.Result;
+
+namespace Application.Core.Api.Result;
+
+/// <summary>
+/// Represents a problem details result.
+/// </summary>
+public record ProblemDetails : Error
+{
+    /// <summary>
+    /// Creates a new instance of <see cref="ProblemDetails"/>.
+    /// </summary>
+    /// <param name="message">The error message.</param>
+    /// <param name="status">The status.</param>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// Exception thrown when the <paramref name="status"/> is not in the expected problem details range.
+    /// </exception>
+    public ProblemDetails(string message, int status = 500) : base(message)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(Status, 400);
+        ArgumentOutOfRangeException.ThrowIfGreaterThanOrEqual(Status, 600);
+        Status = status;
+    }
+    /// <summary>
+    /// Gets or initializes the type.
+    /// </summary>
+    public string? Type { get; init; }
+    
+    /// <summary>
+    /// Gets or initializes the title.
+    /// </summary>
+    public string? Title { get; init; }
+    
+    /// <summary>
+    /// Gets or initializes the detail.
+    /// </summary>
+    public string? Detail { get; init; }
+    
+    /// <summary>
+    /// Gets or initializes the instance.
+    /// </summary>
+    public string? Instance { get; init; }
+    
+    /// <summary>
+    /// The status code.
+    /// </summary>
+    public int Status { get; }
+}
