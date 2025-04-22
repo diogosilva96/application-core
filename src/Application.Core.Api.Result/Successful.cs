baf2d2494915ca78ce08a97040b73f98200eab38
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-22 20:24:27 +0100

    Add TODO

--- src/Application.Core.Api.Result/Successful.cs
+++ src/Application.Core.Api.Result/Successful.cs
@@ -4,4 +4,5 @@
 /// Represents a successful result.
 /// </summary>
 /// <param name="Value">The optional value.</param>
+// TODO: check how we'd rather pass the headers in case we need to add custom headers to a response.
 public record Successful(object? Value = null);
