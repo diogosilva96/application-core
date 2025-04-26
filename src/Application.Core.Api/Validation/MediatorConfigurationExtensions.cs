be5cf92084792053564a6ef64f9619c259e28c47
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-26 21:33:10 +0100

    Introduce ValidationBehavior & MediatorConfiguration extension method for registering it

---
+++ src/Application.Core.Api/Validation/MediatorConfigurationExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Application.Core.Mediator;
+
+namespace Application.Core.Api.Validation;
+
+/// <summary>
+/// Class that contains extension methods for <see cref="MediatorConfiguration" />.
+/// </summary>
+public static class MediatorConfigurationExtensions
+{
+    /// <summary>
+    /// Adds the validation behavior to the given <paramref name="mediatorConfiguration" />.
+    /// </summary>
+    /// <param name="mediatorConfiguration">The mediator configuration to add the behavior for.</param>
+    /// <returns>The <see cref="MediatorConfiguration" /> with the configured validation behavior.</returns>
+    public static MediatorConfiguration AddValidationBehavior(this MediatorConfiguration mediatorConfiguration) =>
+        mediatorConfiguration.AddBehavior(typeof(ValidationBehavior<,>));
+}
---
+++ src/Application.Core.Api/Validation/ValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using Application.Core.Api.Result;
+using Application.Core.Mediator;
+using FluentValidation;
+
+namespace Application.Core.Api.Validation;
+
+/// <summary>
+/// Represents a behavior that validates requests.
+/// </summary>
+/// <param name="validators">The request validators.</param>
+/// <typeparam name="TRequest">The request type.</typeparam>
+/// <typeparam name="TResponse">The response type.</typeparam>
+public class ValidationBehavior<TRequest, TResponse>(IEnumerable<IValidator<TRequest>> validators)
+    : IBehavior<TRequest, TResponse> where TRequest : IRequest<TResponse> where TResponse : ApiResult
+{
+    /// <inheritdoc />
+    public async Task<TResponse> HandleAsync(TRequest request, Func<Task<TResponse>> next, CancellationToken cancellationToken = default)
+    {
+        var validationTasks = validators.Select(x => x.ValidateAsync(request, cancellationToken))
+                                        .ToArray();
+
+        await Task.WhenAll(validationTasks);
+        var validationErrors = validationTasks.Where(t => !t.Result.IsValid)
+                                              .SelectMany(t => t.Result.Errors)
+                                              .ToArray();
+        if (validationErrors.Length > 0)
+        {
+            return (TResponse)new BadRequest($"Validation failed for request of type '{typeof(TRequest).Name}'.", validationErrors);
+        }
+
+        return await next();
+    }
+}
