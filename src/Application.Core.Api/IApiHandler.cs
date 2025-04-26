f0f38baac8f35e7fc1b08b9c6afe5fdef22a93b9
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-26 21:40:00 +0100

    Introduce IApiHandler abstraction

---
+++ src/Application.Core.Api/IApiHandler.cs
@@ -0,0 +1,10 @@
+﻿using Application.Core.Api.Result;
+using Application.Core.Mediator;
+
+namespace Application.Core.Api;
+
+/// <summary>
+/// Represents a handler for processing requests that return results of type <see cref="ApiResult"/>.
+/// </summary>
+/// <typeparam name="TRequest">The request type.</typeparam>
+public interface IApiHandler<in TRequest> : IHandler<TRequest, ApiResult> where TRequest : IRequest<ApiResult>;
