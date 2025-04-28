ad769dca88213f5dda9bf6017e27a8a59c8c65ae
diogosilva96 <diogo.silva.96@outlook.com>
2025-04-28 19:37:25 +0100

    Add unit tests for MediatorConfigurationExtensions

---
+++ tests/Application.Core.Api.UnitTests/Validation/MediatorConfigurationExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Application.Core.Api.Validation;
+using Application.Core.Mediator;
+
+namespace Application.Core.Api.UnitTests.Validation;
+
+public class MediatorConfigurationExtensionsTests
+{
+    [Fact]
+    public void AddValidationBehavior_AddsValidationBehaviorToServicesToRegister()
+    {
+        // Arrange
+        var mediatorConfiguration = new MediatorConfiguration();
+
+        // Act
+        var result = mediatorConfiguration.AddValidationBehavior();
+
+        // Assert
+        Assert.Contains(result.ServicesToRegister, service => service.ServiceType == typeof(IBehavior<,>) &&
+                                                              service.ImplementationType == typeof(ValidationBehavior<,>));
+    }
+}
