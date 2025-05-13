4bf8e33090a2e7d1bca3fef900048e8babc8fbc4
diogosilva96 <diogo.silva.96@outlook.com>
2025-05-13 19:15:52 +0100

    Add extension method `WithValidationFailurePropertyMapper` for RouteHandlerBuilder.
    This allows to specify the property mapper to use for the endpoint being configured on the route handler builder

---
+++ src/Application.Core.Api/Validation/RouteHandlerBuilderExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Application.Core.Api.Validation;
+
+/// <summary>
+/// Class that contains extension methods for <see cref="RouteHandlerBuilder" />.
+/// </summary>
+public static class RouteHandlerBuilderExtensions
+{
+    /// <summary>
+    /// Adds the <see cref="ValidationFailurePropertyMapperBase" /> to the given <paramref name="builder" />.
+    /// </summary>
+    /// <param name="builder">The builder to add the property mapper to.</param>
+    /// <typeparam name="TMapper">The mapper type.</typeparam>
+    /// <returns>The configured <see cref="RouteHandlerBuilder"/>.</returns>
+    public static RouteHandlerBuilder WithValidationFailurePropertyMapper<TMapper>(this RouteHandlerBuilder builder) 
+        where TMapper : class, IValidationFailurePropertyMapper
+    {
+        return builder.WithMetadata(new ValidationFailurePropertyMapperBinding(typeof(TMapper)));
+    }
+}
