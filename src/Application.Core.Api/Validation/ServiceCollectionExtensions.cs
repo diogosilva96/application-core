db14aa863bc3f8f0a08c95762b19a1d4389df6b3
diogosilva96 <diogo.silva.96@outlook.com>
2025-05-13 19:28:11 +0100

    Add `AddValidationFailureMapping` extension method to simplify the registration of validation failure mappers and property mappers

---
+++ src/Application.Core.Api/Validation/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Core.Api.Validation;
+
+/// <summary>
+/// Class that contains extension methods for <see cref="IServiceCollection" />
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    private static readonly Type _mapperAbstractionType = typeof(IValidationFailurePropertyMapper);
+
+    /// <summary>
+    /// Adds the required services for validation failure mapping to the specified <see cref="IServiceCollection" />.
+    /// </summary>
+    /// <param name="serviceCollection"> The service collection to add the services for.</param>
+    /// <param name="assemblies">The assemblies containing the implementation for the<see cref="IValidationFailurePropertyMapper" />.</param>
+    /// <returns>The service collection with the added services.</returns>
+    public static IServiceCollection AddValidationFailureMapping(this IServiceCollection serviceCollection, params Assembly[] assemblies)
+    {
+        ArgumentNullException.ThrowIfNull(serviceCollection);
+
+        foreach (var (interfaceType, implementationType) in ScanPropertyMapperFromAssemblies(assemblies))
+        {
+            serviceCollection.AddSingleton(interfaceType, implementationType);
+        }
+
+        return serviceCollection.AddScoped<IValidationFailureMapper, ValidationFailureMapper>()
+                                .AddHttpContextAccessor();
+    }
+
+    private static (Type InterfaceType, Type ImplementationType)[] ScanPropertyMapperFromAssemblies(params Assembly[] assemblies) =>
+        assemblies.SelectMany(a => a.GetTypes().Where(t => t is { IsAbstract: false, IsInterface: false } &&
+                                                           t.GetInterfaces().Any(it =>
+                                                               it.IsGenericType && it.GetGenericTypeDefinition() == _mapperAbstractionType)))
+                  .Select(t => new ValueTuple<Type, Type>(
+                      t.GetInterfaces().First(it => it.IsGenericType && it.GetGenericTypeDefinition() == _mapperAbstractionType), t))
+                  .ToArray();
+}
