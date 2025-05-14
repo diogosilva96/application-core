8e123b1f18cc0e3446ae79989a5f85e284706867
diogosilva96 <diogo.silva.96@outlook.com>
2025-05-14 20:42:30 +0100

    Add unit tests for `AddValidationFailureMapping`

---
+++ tests/Application.Core.Api.UnitTests/Validation/ServiceCollectionExtensionsTests.cs
@@ -0,0 +1,46 @@
+﻿using Application.Core.Api.UnitTests.Utils;
+using Application.Core.Api.Validation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Core.Api.UnitTests.Validation;
+
+public class ServiceCollectionExtensionsTests
+{
+    private readonly ServiceCollection _serviceCollection = [];
+
+    [Fact]
+    public void AddValidationFailureMapping_ShouldRegisterIValidationFailureMapper()
+    {
+        // Act
+        _serviceCollection.AddValidationFailureMapping();
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+        // Assert
+        Assert.NotNull(serviceProvider.GetService<IValidationFailureMapper>());
+    }
+
+    [Fact]
+    public void AddValidationFailureMapping_ShouldRegisterHttpContextAccessor()
+    {
+        // Act
+        _serviceCollection.AddValidationFailureMapping();
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+        // Assert
+        Assert.NotNull(serviceProvider.GetService<IHttpContextAccessor>());
+    }
+
+    [Fact]
+    public void AddValidationFailureMapping_ShouldRegisterMappersFromAssemblies()
+    {
+        // Act
+        _serviceCollection.AddValidationFailureMapping(typeof(TestRequestValidationFailurePropertyMapper).Assembly);
+        var serviceProvider = _serviceCollection.BuildServiceProvider();
+
+        // Assert
+        var mappers = serviceProvider.GetServices<IValidationFailurePropertyMapper>().ToArray();
+        Assert.NotEmpty(mappers);
+        Assert.Contains(mappers, mapper => mapper.GetType() == typeof(TestRequestValidationFailurePropertyMapper));
+    }
+}
