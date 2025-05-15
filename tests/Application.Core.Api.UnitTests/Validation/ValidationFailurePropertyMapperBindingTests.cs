805e7b9747a81ff7a9d1f91a8f1d4d483e77b6d8
diogosilva96 <diogo.silva.96@outlook.com>
2025-05-15 18:24:30 +0100

    Add unit tests for ValidationFailurePropertyMapperBinding

---
+++ tests/Application.Core.Api.UnitTests/Validation/ValidationFailurePropertyMapperBindingTests.cs
@@ -0,0 +1,30 @@
+﻿using Application.Core.Api.UnitTests.Utils;
+using Application.Core.Api.Validation;
+
+namespace Application.Core.Api.UnitTests.Validation;
+
+public class ValidationFailurePropertyMapperBindingTests
+{
+    [Fact]
+    public void Constructor_ShouldCreateInstance_WithValidMapperType()
+    {
+        // Arrange
+        var mapperType = typeof(TestRequestValidationFailurePropertyMapper);
+
+        // Act
+        var binding = new ValidationFailurePropertyMapperBinding(mapperType);
+
+        // Assert 
+        Assert.Equal(mapperType, binding.MapperType);
+    }
+
+    [Fact]
+    public void Constructor_ShouldThrowArgumentException_WithNonMapperType()
+    {
+        // Arrange
+        var invalidType = typeof(TestRequest);
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => new ValidationFailurePropertyMapperBinding(invalidType));
+    }
+}
