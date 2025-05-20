9a3fdc64734939b85ed18a429fca42b24463a7cf
diogosilva96 <diogo.silva.96@outlook.com>
2025-05-20 18:35:16 +0100

    Add Logging to ApiRequestProcessor

--- src/Application.Core.Api/RequestProcessing/ApiRequestProcessor.cs
+++ src/Application.Core.Api/RequestProcessing/ApiRequestProcessor.cs
@@ -2,6 +2,7 @@
 using Application.Core.Api.Result.Mapping;
 using Application.Core.Mediator;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Core.Api.RequestProcessing;
 
@@ -10,13 +11,17 @@ namespace Application.Core.Api.RequestProcessing;
 /// </summary>
 /// <param name="sender">The sender.</param>
 /// <param name="resultMapper">The result mapper.</param>
-internal class ApiRequestProcessor(ISender sender, IApiResultMapper resultMapper) : IApiRequestProcessor
+internal class ApiRequestProcessor(ISender sender, IApiResultMapper resultMapper, ILogger<ApiRequestProcessor> logger) : IApiRequestProcessor
 {
     /// <inheritdoc />
     public async Task<IResult> ProcessAsync<TRequest>(TRequest request, CancellationToken cancellationToken = default)
         where TRequest : IRequest<ApiResult>
     {
         var apiResult = await sender.SendAsync(request, cancellationToken);
+        
+        apiResult.Switch(success => logger.LogInformation("{RequestType} resulted in {SuccessResultType}", request.GetType().Name, success.GetType().Name),
+            problemDetails => logger.LogWarning("{RequestType} resulted in {FailureResultType}. Details: {Details}", request.GetType().Name, problemDetails.GetType().Name, problemDetails.ToDetailedErrorMessage()));
+        
         return resultMapper.Map(apiResult);
     }
 }
