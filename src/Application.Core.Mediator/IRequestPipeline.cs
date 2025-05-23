8030c3cb080be070c0c48b9a7ea01a8b223de1ed
diogosilva96 <diogo.silva.96@outlook.com>
2025-05-23 19:37:30 +0100

    Introduce RequestPipeline concept

---
+++ src/Application.Core.Mediator/IRequestPipeline.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Core.Mediator;
+
+/// <summary>
+/// Represents a request pipeline abstraction.
+/// </summary>
+/// <typeparam name="TRequest">The type of the request. Must implement <see cref="IRequest{TResponse}"/>.</typeparam>
+/// <typeparam name="TResponse">The type of the response.</typeparam>
+public interface IRequestPipeline<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    /// <summary>
+    /// Executes the processing pipeline for the given request.
+    /// </summary>
+    /// <typeparam name="TRequest">The type of the request.</typeparam>
+    /// <typeparam name="TResponse">The type of the response.</typeparam>
+    /// <param name="request">The request object to be processed by the pipeline.</param>
+    /// <param name="cancellationToken">The cancellation token used to observe cancellation requests.</param>
+    /// <returns>The resulting response.<typeparamref name="TResponse"/>.</returns>
+    public Task<TResponse> ExecuteAsync(TRequest request, CancellationToken cancellationToken = default);
+}
---
+++ src/Application.Core.Mediator/RequestPipeline.cs
@@ -0,0 +1,35 @@
+﻿namespace Application.Core.Mediator;
+/// <summary>
+/// Represents a request pipeline component.
+/// </summary>
+/// <param name="behaviors">The behaviors to use.</param>
+/// <param name="handler">The handler to use.</param>
+/// <typeparam name="TRequest">The type of the request. Must implement <see cref="IRequest{TResponse}"/>.</typeparam>
+/// <typeparam name="TResponse">The type of the response.</typeparam>
+internal class RequestPipeline<TRequest, TResponse>(
+    IEnumerable<IBehavior<TRequest, TResponse>> behaviors, 
+    IHandler<TRequest, TResponse> handler) 
+    : IRequestPipeline<TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    private readonly IBehavior<TRequest, TResponse>[] _behaviors = behaviors.ToArray();
+    
+    /// <inheritdoc />
+    public Task<TResponse> ExecuteAsync(TRequest request, CancellationToken cancellationToken = default)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        
+        return ExecuteInternalAsync(0);
+        
+        Task<TResponse> ExecuteInternalAsync(int index)
+        {
+            var behaviorsSlice = _behaviors.AsSpan()[index..];
+            // ReSharper disable once ConvertIfStatementToReturnStatement
+            if (behaviorsSlice.Length == 0)
+            {
+                return handler.HandleAsync(request, cancellationToken);
+            }
+
+            return behaviorsSlice[0].HandleAsync(request, () => ExecuteInternalAsync(index + 1), cancellationToken);
+        }
+    }
+}
